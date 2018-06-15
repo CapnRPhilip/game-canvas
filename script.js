13c5050eba91587bd7d1811efcfbe72ac9bd3c74
Valindo Godinho <valindo.godinho@gmail.com>
2018-06-15 15:37:58 +0530

    Got the left case to work, not working well but good enough to work with for now

--- script.js
+++ script.js
@@ -4,48 +4,81 @@ var h = 20;
 var w = 20;
 var x = 0;
 var y = 0;
+var a = ""
+var b = ""
 
 var object_1 = {
   x1: 100,
   x2: 100,
-  y1: 50,
-  y2: 50
+  side: 50
 };
 
 function keydownhandler(e) {
+
   switch (e.keyCode) {
+    
     case 37:
-    console.log("I reach here")
-    if(x>0){
-        console.log("I reach here 2")
-        if(!(x<object_1.x1+object_1.y1 && y+h>object_1.x2 && y<object_1.x2+object_1.y2)){
-                x -= 10;
+      if (x > 0) {
+        console.log("LEFT");
+        console.log('y > object_1.x2+object_1.side : ', y > object_1.x2+object_1.side);
+        console.log("y < object_1.x2 : ", y < object_1.x2);
+        console.log('y : ',y )
+        console.log('!(y > object_1.x2+object_1.side || y < object_1.x2 ) : ',!(y > object_1.x2+object_1.side || y < object_1.x2 ) )
+        if(!(y > object_1.x2+object_1.side || y < object_1.x2 )){
+          if(!(x == object_1.x1 + object_1.side)){
+            x-=10
+          }
+        } else {
+          x-=10
         }
-    }
+        
+        
+      }
       break;
 
     case 38:
-    if(y>0){
-        if(!(y<object_1.x2+object_1.y2 && x+w>object_1.x1 && x<object_1.x1+object_1.y1)){
-        y -= 10;
+      if (y > 0) {
+        console.log("UP");
+        if (
+          !(
+            y < object_1.x2 + object_1.side &&
+            x + w > object_1.x1 &&
+            x < object_1.x1 + object_1.side
+          )
+        ) {
+          y -= 10;
         }
-    }
+      }
       break;
 
     case 39:
-    if(x+w<650){
-        if(!(x+w>object_1.x1 && y+h>object_1.x2 && y<object_1.x2+object_1.y2)){
-          x += 10;  
-       }
-    }
+      if (x + w < 650) {
+        console.log("RIGHT");
+        if (
+          !(
+            x + w > object_1.x1 &&
+            y + h > object_1.x2 &&
+            y < object_1.x2 + object_1.side
+          )
+        ) {
+          x += 10;
+        }
+      }
 
       break;
     case 40:
-    if(y+h<650){
-        if(!(y+h>object_1.x2 && x+w>object_1.x1 && x<object_1.x1+object_1.y1)){
-        y += 10;
+      if (y + h < 650) {
+        console.log("DOWN");
+        if (
+          !(
+            y + h > object_1.x2 &&
+            x + w > object_1.x1 &&
+            x < object_1.x1 + object_1.side
+          )
+        ) {
+          y += 10;
         }
-    }
+      }
       break;
   }
   draw();
@@ -54,12 +87,12 @@ function keydownhandler(e) {
 function drawbox() {
   ctx.beginPath();
   ctx.rect(x, y, h, w);
-  ctx.strokeStyle = "rgba(0, 0, 0, 1.0)";
+  a = ctx.strokeStyle = "rgba(0, 0, 0, 1.0)";
   ctx.stroke();
   ctx.closePath();
 
   ctx.beginPath();
-  ctx.rect(object_1.x1, object_1.x2, object_1.y1, object_1.y2);
+  b = ctx.rect(object_1.x1, object_1.x2, object_1.side, object_1.side);
   ctx.fillStyle = "#FF0000";
   ctx.fill();
   ctx.closePath();
