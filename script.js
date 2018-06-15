9e7a0421a18423b0261b9b2ec708cbeefaa87c1b
CapnRPhilip <40234964+CapnRPhilip@users.noreply.github.com>
2018-06-15 11:59:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var h = 30;
-var w = 30;
+var h = 20;
+var w = 20;
 var x = 0;
 var y = 0;
-var r_press = false;
-var l_press = false;
-var u_press = false;
-var d_press = false;
 
 var object_1 = {
   x1: 100,
-  x2: 20,
-  y1: 40,
-  y2: 30
+  x2: 100,
+  y1: 50,
+  y2: 50
 };
 
 function keydownhandler(e) {
   switch (e.keyCode) {
     case 37:
-      x -= 10;
+    console.log("I reach here")
+    if(x>0){
+        console.log("I reach here 2")
+        if(!(x<object_1.x1+object_1.y1 && y+h>object_1.x2 && y<object_1.x2+object_1.y2)){
+                x -= 10;
+        }
+    }
       break;
+
     case 38:
-      y -= 10;
+    if(y>0){
+        if(!(y<object_1.x2+object_1.y2 && x+w>object_1.x1 && x<object_1.x1+object_1.y1)){
+        y -= 10;
+        }
+    }
       break;
+
     case 39:
-      console.log(x);
-      x + w > 400 || x + w == 400 ? null : (x += 10);
+    if(x+w<650){
+        if(!(x+w>object_1.x1 && y+h>object_1.x2 && y<object_1.x2+object_1.y2)){
+          x += 10;  
+       }
+    }
 
       break;
     case 40:
-      y + h > 400 || y + h == 400 ? null : (y += 10);
+    if(y+h<650){
+        if(!(y+h>object_1.x2 && x+w>object_1.x1 && x<object_1.x1+object_1.y1)){
+        y += 10;
+        }
+    }
       break;
   }
   draw();
@@ -51,7 +66,7 @@ function drawbox() {
 }
 
 function draw() {
-  ctx.clearRect(0, 0, 400, 400);
+  ctx.clearRect(0, 0, 650, 650);
   drawbox();
 }
 
